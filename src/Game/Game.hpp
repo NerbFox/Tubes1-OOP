7b0e774944eebbb35d28eac0f64847719d83e433
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-03-11 18:13:18 +0700

    src/Game : attribute

--- src/Game/Game.hpp
+++ src/Game/Game.hpp
@@ -3,13 +3,30 @@
 
 #include <queue>
 #include <iostream>
-#include "Player/Player.hpp"
+#include "../Player/Player.hpp"
+#include "../Inventory/TableCard.hpp"
+#include "../Inventory/DeckCard.hpp"
 
 using namespace std;
 
 class Game {
     private :
-        queue<Player> playersState; 
+        queue<Player> playerState;
+        TableCard tableCard;
+        DeckCard deckCard;
+        int reward;
+        int countPermainan;
+        int countRonde;
+
+        const int MAX_PLAYER = 7;
+        // const int 
+
+    public : 
+        Game() : deckCard(), tableCard(){
+            for (int i = 0; i < this->MAX_PLAYER; i++) {
+                Player *p 
+            }
+        }
         
     
 };  
