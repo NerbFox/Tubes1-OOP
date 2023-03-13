b418577a327fd1060f5979db36104fdef1e4b2db
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-13 11:07:50 +0700

    Update beberapa ability

--- src/Ability/ReRoll.cpp
+++ src/Ability/ReRoll.cpp
@@ -1,1 +1,12 @@
 #include "../lib-header/Ability/ReRoll.hpp"
+
+ReRoll::ReRoll() {
+    Ability::setType("ReRoll");
+}
+
+// action
+void ReRoll::action(Game game) {
+    // Throw 2 cards from player's main deck and take 2 cards again
+    // game.playerPointer->throwCard(2);
+    // game.playerPointer->takeCard(2);
+}
--- src/lib-header/Command/Ability.hpp
+++ src/lib-header/Command/Ability.hpp
@@ -1,7 +1,15 @@
 #ifndef ABILITY_HPP
 #define ABILITY_HPP
+#include <string>
+#include "../Command/Command.hpp"
+using namespace std;
 
-class Ability {
+class Ability : public Command {
+    /*Kartu Ability akan diberikan ke pemain pada ronde kedua. Pemain dapat menggunakan ability dan 
+    melihat kartu ability pada giliran kedua. Kartu ability hanya ada tujuh sehingga setiap 
+    permainan pasti akan habis dibagi ke semua pemain. Kartu ability hanya bisa dipakai sekali per 
+    permainan dan bisa juga tidak digunakan. Terdapat kekuatan diantaranya.
+    */
     private:
         string type;
     public:
@@ -9,6 +17,13 @@ class Ability {
         Ability(const Ability& otherAbility);
         Ability& operator=(const Ability& otherAbility);
         ~Ability();
+        // getter
+        string getType();
+        // setter
+        void setType(string type);
+        // action
+        virtual void action(Game) = 0;
+        virtual void action(Player) = 0;
 };  
 
 
