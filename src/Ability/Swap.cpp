b696061cb56c8268e3e18fe2d086fcef99408a99
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-13 17:33:25 +0700

    Update Swap.cpp

--- src/Ability/Swap.cpp
+++ src/Ability/Swap.cpp
@@ -1,1 +1,16 @@
 #include "../lib-header/Ability/Swap.hpp"
+
+Swap::Swap() {
+    Ability::setType("Swap");
+}
+
+// action
+void Swap::action(Game game){
+    /* menukar 1 kartu main deck milik pemain lain dengan 1 kartu main deck milik pemain lain. Tidak 
+    boleh ditukar dengan kartu main deck diri sendiri. */
+    cout << "Swap diaktifkan" << endl;
+    cout << "Pilih 2 pemain yang ingin ditukar kartunya: " << endl;
+    
+
+    
+}
