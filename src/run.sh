4289e2728e0bc441b967cb4ba1a51e1243337e02
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-14 14:50:28 +0700

    Create run.sh

---
+++ src/run.sh
@@ -0,0 +1,6 @@
+#  Compile main.cpp and all libraries
+g++ main.cpp ./Ability/*.cpp ./Card/*.cpp ./Command/*.cpp ./ExceptionHandler/*.cpp ./Game/*.cpp ./Player/*.cpp ./Inventory/*.cpp
+-o main
+
+#  Run main.cpp and all libraries
+./main
