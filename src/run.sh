007e7f2ded65b9827003d4aa665371608f3a2447
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-14 15:08:38 +0700

    Update

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
 #  Compile main.cpp and all libraries
-g++ main.cpp ./Ability/*.cpp ./Card/*.cpp ./Command/*.cpp ./ExceptionHandler/*.cpp ./Game/*.cpp ./Player/*.cpp ./Inventory/*.cpp
+g++ ./main.cpp ./Ability/*.cpp ./Card/*.cpp ./Command/*.cpp ./ExceptionHandler/*.cpp ./Game/*.cpp ./Player/*.cpp ./Inventory/*.cpp
 -o main
 
 #  Run main.cpp and all libraries
