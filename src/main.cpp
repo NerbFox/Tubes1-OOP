8d6ad788cafdf668c5dd2aa0f7fc31a1febb230a
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-16 01:48:21 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include "lib-header/Game/Game.hpp"
 using namespace std;
 
+// template to get max from array
+// template <class T1, class Type>
+// T1 getMax(T1 Container){
+//   T1::iterator it = Container.begin();
+//   Type max = *it;
+//   for (it = Container.begin(); it != Container.end(); it++){
+//     if (*it > max){
+//       max = *it;
+//     }
+//   }
+//   return max;
+// }
+
+
 int main() {
+  // int arr[] = {1, 2, 3, 4, 5};
+  // cout << getMax<int*, int>(arr);
   try {
     Game gameState;
     
