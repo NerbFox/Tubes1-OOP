b8ac0ba3f8c80a0fe7351a626d12841023b6d99f
NerbSerg <93074692+NerbFox@users.noreply.github.com>
2023-03-16 13:45:59 +0700

    Update print cards

---
+++ src/Card/colors.hpp
@@ -0,0 +1,17 @@
+#ifndef COLORS_HPP
+#define COLORS_HPP
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+// color in string ANSCII
+const string RED = "\033[1;31m";
+const string GREEN = "\033[1;32m";
+const string YELLOW = "\033[1;33m";
+const string BLUE = "\033[1;34m";
+const string MAGENTA = "\033[1;35m";
+const string CYAN = "\033[1;36m";
+const string WHITE = "\033[1;37m";
+const string RESET = "\033[0m";
+
+#endif
